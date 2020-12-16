19aa2e70f0066c09ca20754b6c7eed361a566e60
HasinduLanka <hlrathnayaka@gmail.com>
2020-12-16 21:49:22 +0530

    git intructions

--- install.essentials.sh
+++ install.essentials.sh
@@ -8,4 +8,5 @@ chmod +x ./fs/home/*.sh
 sudo apt update
 sudo apt install --assume-yes fdisk g++ nano screen gedit git wget curl ark stacer resolvconf ufw gufw make 
 
- 
+git config pull.rebase true 
+echo configure git like    git config --global user.name "NAME" user.email "ME@MAIL.COM"
