c9bd5da961e7bbf4f73518cb947a35f43f294eca
HasinduLanka <hlrathnayaka@gmail.com>
2020-12-17 12:22:42 +0530

    dotnet from apt

--- install.apps.sh
+++ install.apps.sh
@@ -9,8 +9,8 @@ echo SnapD is installed. But you should logout and login again to use it
 #while read -r env; do export "$env"; done
 
 sudo snap install code --classic
-sudo snap install dotnet-sdk --classic --channel=5.0
-sudo snap alias dotnet-sdk.dotnet dotnet
+# sudo snap install dotnet-sdk --classic --channel=5.0
+# sudo snap alias dotnet-sdk.dotnet dotnet
 
 sudo snap install vokoscreen-ng 
 
@@ -18,6 +18,13 @@ sudo snap install vokoscreen-ng
 mkdir sand
 cd sand
 
+wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-3.1 dotnet-sdk-5.0
+  
 
 wget https://download.xnview.com/XnConvert-linux-x64.deb 
 sudo dpkg --install XnConvert-linux-x64.deb  
