3a88afab61e994ab1246512e278f1d65b4e7b3da
HasinduLanka <hlrathnayaka@gmail.com>
2021-01-02 19:02:13 +0530

    -y

--- Apps/Python_PIP.sh
+++ Apps/Python_PIP.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 # sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
-sudo apt install python3.8 
-sudo apt install python3.9 
+sudo apt install -y  python3.8 
+sudo apt install -y  python3.9 
 
-sudo apt install python3-pip
+sudo apt install -y  python3-pip
