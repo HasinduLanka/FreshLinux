ea1ce3c5258f8bd19ce76f3ca19238421ee3b4a3
HasinduLanka <hlrathnayaka@gmail.com>
2021-02-06 11:08:42 +0530

    kisak-mesa

--- Apps/mesa-gpu-drivers.sh
+++ Apps/mesa-gpu-drivers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo add-apt-repository -y ppa:oibaf/graphics-drivers
+sudo add-apt-repository -y ppa:kisak/kisak-mesa
 sudo apt update
+sudo apt install mesa
 sudo apt -y dist-upgrade 
