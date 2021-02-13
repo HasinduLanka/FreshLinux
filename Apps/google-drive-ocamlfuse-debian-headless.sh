16f8127d20a2754e0c6108c285048b7bf62f61d2
HasinduLanka <hlrathnayaka@gmail.com>
2021-02-13 12:09:05 +0530

    Headless

--- Apps/google-drive-ocamlfuse-debian-headless.sh
+++ Apps/google-drive-ocamlfuse-debian-headless.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Do not run this in a User Interactive machine. This will break your /bin/xdg-open "
+
 sudo apt install -y software-properties-common dirmngr gnupg-agent
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AD5F235DF639B041
 echo 'deb http://ppa.launchpad.net/alessandro-strada/ppa/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/alessandro-strada-ubuntu-ppa.list >/dev/null
@@ -28,3 +30,20 @@ echo "pkill -c -f -9 google-drive-ocamlfuse ; fusermount -u ~/GoogleDrive " > ~/
 
 chmod +x ~/google-drive-fuse-start.sh
 chmod +x ~/google-drive-fuse-stop.sh
+
+echo
+echo "You are just one keypress away from ruining up your Operating system"
+echo "Following functionality is designed only for Authorizing Headless computers (with no GUI access) as they have no xdg-open installed"
+echo "Press [Enter] to replace your /bin/xdg-open with a dummy script"
+echo "Do this only if you know what your are doing"
+echo "You can recover your original /bin/xdg-open from /bin/xdg-open.back"
+echo "Press CTRL+C to cancel this"
+read line
+
+
+sudo cp /bin/xdg-open /bin/xdg-open.back
+
+sudo bash -c 'echo -e $"#!/bin/sh\necho $* > /dev/stderr" > /bin/xdg-open'
+chmod +x /bin/xdg-open
+
+echo "You can autorize by running  ~/google-drive-fuse-start.sh "
