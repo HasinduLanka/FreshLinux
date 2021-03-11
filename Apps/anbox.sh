0a724e21f1a1acc3836577d68ad27ed555332504
HasinduLanka <hlrathnayaka@gmail.com>
2021-03-11 16:54:31 +0530

    anbox

---
+++ Apps/anbox.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install dbus-x11 android-sdk-platform-tools-common 
+sudo snap install --beta --devmode anbox
+
+echo Desktop Files are stored at 
+echo ~/snap/anbox/common/app-data/applications/anbox/
+
