ba2dd6eddba275d4c4d28dcbcc96b2a425684dee
HasinduLanka <hlrathnayaka@gmail.com>
2021-04-21 18:29:29 +0530

    qml-module-qt-labs-platform

--- config.desktop.sh
+++ config.desktop.sh
@@ -2,7 +2,7 @@
 
 sudo gpasswd -a $USER input
 
-sudo apt -y install wmctrl xdotool libinput-tools  gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0 pulseaudio 
+sudo apt -y install wmctrl xdotool libinput-tools  gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0 pulseaudio qml-module-qt-labs-platform 
 
 
 
