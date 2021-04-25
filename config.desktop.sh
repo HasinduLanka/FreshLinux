8a89cd2135226100f43f1e03f534d7fc3dbeb956
HasinduLanka <hlrathnayaka@gmail.com>
2021-04-25 18:48:57 +0530

    brasero

--- config.desktop.sh
+++ config.desktop.sh
@@ -2,7 +2,7 @@
 
 sudo gpasswd -a $USER input
 
-sudo apt -y install filelight wmctrl xdotool libinput-tools  gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0 pulseaudio scrot kde-spectacle kdialog qml-module-qt-labs-platform 
+sudo apt -y install filelight brasero wmctrl xdotool libinput-tools  gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0 pulseaudio scrot kde-spectacle kdialog qml-module-qt-labs-platform 
 
 
 
