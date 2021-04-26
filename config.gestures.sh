c24aceec3e4064e0598d05bcedaa179a8cc3bfec
HasinduLanka <hlrathnayaka@gmail.com>
2021-04-26 22:36:13 +0530

    force the use of latest AMDGPU drivers

--- config.gestures.sh
+++ config.gestures.sh
@@ -5,13 +5,12 @@ sudo gpasswd -a $USER input
 sudo apt -y install wmctrl xdotool libinput-tools  gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0 
 
 
-chmod +x ./fs/home/*.sh
+chmod +x ./fs/home/.config/scripts/*
 
-rsync -a ./fs/home/ ~/
+rsync -a ./fs/home/.config ~/.config
 
 
-sudo mkdir sand
-sudo chown $USER sand
+mkdir -p sand
 cd sand
 git clone https://github.com/bulletmark/libinput-gestures.git
 cd libinput-gestures
